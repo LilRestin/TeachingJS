729bf5306ee37ffca75208e18dbe963dc86c8b12
LilRestin <56673960+LilRestin@users.noreply.github.com>
2021-03-02 08:43:47 +0300

    Add files via upload

---
+++ JS/if.js
@@ -0,0 +1,90 @@
+// let = year = prompt('В каком году вы родились?')
+// if (year < 2001) {
+//     alert('Вы правы!')
+//     alert('Хорош!')
+// }   else if (year > 2001) {
+//         alert('Error')
+//     }
+//     else {
+//         alert('Верно!')
+//     }
+
+
+// let number 
+// let age = prompt('Сколько?')
+// if (age == 19) {
+//     number = true;
+// } else {
+//     number = false;
+// }
+// alert(number)
+
+// let age = prompt('Сколько?')
+// let number = (age == 19) ? true : false;
+// let number = age == 19 ? true : false;
+// let number = age == 19
+// alert(number)
+
+
+// let age = prompt('Возраст?', 18)
+// let message = (age < 3) ? 'Привет, малыш!': (age < 18) ? 'Привет!': (age < 100) ? 'Здравствуйте!': 'Пожилой олдмэн'
+// alert(message);
+
+
+// let company = prompt('Какая компания создала JavaScript?', '');
+// (company == 'Netscape') ?
+//    alert('Верно!') : alert('Неправильно.')
+
+   
+// let company = prompt('Какая компания создала JavaScript?')
+// if (company == 'Netscape') {
+//     alert('Верно!')
+// }   else {
+//     alert('Неправильно!')
+// }
+
+
+// let off = prompt('Какое офицальное название у JavaScript?');
+// if (off == 'ECMAScript') 
+// {
+//     alert('Верно!')
+// }
+// else
+// {
+//     alert('Не знаете? ECMAScript!')
+// }
+
+
+// let number = prompt('Введите число:');
+// if (number > 0)
+// {
+//     alert('1')
+// }
+// else if (number < 0)
+// {
+//     alert('-1')
+// }
+// else
+// {
+//     alert("0")
+// }
+
+
+// let result;
+// let a = prompt('Введите А');
+// let b = prompt('Введите B');
+// result = (a + b < 4) ? alert('Мало') : alert('Много');
+
+
+// result = (a + b < 4) ? 'Мало' : 'Много';
+
+
+let = login = prompt('Вы кто?');
+let message = (login == 'Сотрудник') ? 'Привет' :
+  (login == 'Директор') ? 'Здравствуйте' :
+  (login == '') ? 'Нет логина' :
+  '';
+  alert(message)
+
+
+
---
+++ JS/while.js
@@ -0,0 +1,12 @@
+// let i = 0;
+// while (i <3) {
+//     alert(i);
+//     i++;
+// }
+
+let i = 0;
+do {
+    alert(i)
+    i++;
+} while (i < 3)
+
